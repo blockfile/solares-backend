8c629f09bc828586c03df2eeb74403f98a9f9f1c
Ivan <virusbsc.dev00@gmail.com>
2026-03-28 23:29:48 +0800

    fixes

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const auditRoutes = require("./src/routes/audit.routes");
 
 const app = express();
 
+app.set("trust proxy", 1);
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(helmet());
