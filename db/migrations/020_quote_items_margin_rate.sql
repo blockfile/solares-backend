99c4af8bef36f2d26ddef66b92edc85529b3f6e8
Ivan <virusbsc.dev00@gmail.com>
2026-04-28 04:05:42 +0800

    fxes

--- db/migrations/020_quote_items_margin_rate.sql
+++ db/migrations/020_quote_items_margin_rate.sql
@@ -1,2 +1,2 @@
 ALTER TABLE quote_items
-  ADD COLUMN margin_rate decimal(8,4) NULL DEFAULT NULL AFTER base_price;
+  ADD COLUMN IF NOT EXISTS margin_rate decimal(8,4) NULL DEFAULT NULL AFTER base_price;
